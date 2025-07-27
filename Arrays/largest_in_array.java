99d99c2a8051c187b5cf26416f32a3e835e4f669
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-07-27 11:22:29 +0530

    problem added

--- Arrays/largest_in_array.java
+++ Arrays/largest_in_array.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
 public class largest_in_array {
     public static void main(String args[]) {
-
+        int arr[] = { 5, 7, 1, 3, 9, 89, 13, 45 };
+        Arrays.sort(arr);
+        System.out.print("largest array is: " + arr[arr.length - 1]);
     }
 }

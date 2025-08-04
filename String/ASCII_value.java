108cbdacbec97e32f50002442dea16468fce0175
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-04 12:46:30 +0530

    day-07

---
+++ String/ASCII_value.java
@@ -0,0 +1,13 @@
+package String;
+
+public class ASCII_value {
+    public static void value(char ch) {
+        int asci = ch;
+        System.out.println("ASCII value of " + ch + " is " + asci);
+    }
+
+    public static void main(String args[]) {
+        char ch = 'e';
+        value(ch);
+    }
+}

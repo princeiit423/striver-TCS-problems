a0bd97d619d0414c18c9ee9a95705441d182e7ec
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-05 12:30:59 +0530

    day-08

---
+++ String/reverse_string.java
@@ -0,0 +1,15 @@
+package String;
+
+public class reverse_string {
+    public static void reverse(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "faiz";
+    }
+}

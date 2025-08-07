4a370c94b21342c778e6e7cc38aee7100ebebd5e
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-07 11:39:17 +0530

    day-10

---
+++ String/maximum_occuring_character.java
@@ -0,0 +1,29 @@
+package String;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class maximum_occuring_character {
+    public static void maxOccur(String s) {
+        int ans = 0;
+        char ansChar = '\0';
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (char key : map.keySet()) {
+            int freq = map.get(key);
+            if (freq > ans) {
+                ans = freq;
+                ansChar = key;
+            }
+        }
+        System.out.println(ansChar + " : " + ans);
+
+    }
+
+    public static void main(String args[]) {
+        String s = "DemonSlayer";
+        maxOccur(s);
+    }
+}

66c278ca19a76ac7219bcf4f6d3a6b644735750b
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-07 12:01:12 +0530

    day-10

--- String/removeDuplicate_in_String.java
+++ String/removeDuplicate_in_String.java
@@ -1,5 +1,8 @@
 package String;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 public class removeDuplicate_in_String {
     public static void removeDup(String s) {
         boolean[] map = new boolean[26];
@@ -13,8 +16,29 @@ public class removeDuplicate_in_String {
         System.out.println(ans);
     }
 
+    public static void hashRemove(String s) {
+        // HashMap<Character, Integer> map = new HashMap<>();
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        String ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0));
+        }
+        for (char key : map.keySet()) {
+
+            if (map.get(key) > 1) {
+                ans += key;
+            } else {
+                ans += key;
+            }
+        }
+        System.out.println(ans);
+    }
+
     public static void main(String[] args) {
         String s = "takeuforward";
         removeDup(s);
+        System.out.println("-----------------------------------");
+        hashRemove(s);
     }
 }

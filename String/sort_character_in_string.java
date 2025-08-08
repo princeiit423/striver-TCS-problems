98fb2b66438130f4f2715361ac7a0db298359067
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-08 12:42:54 +0530

    day-11

--- String/sort_character_in_string.java
+++ String/sort_character_in_string.java
@@ -1,11 +1,17 @@
 package String;
 
+import java.util.Arrays;
+
 public class sort_character_in_string {
     public static void sortChar(String s) {
-
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+        String ans = new String(arr);
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
         String s = "faiz";
+        sortChar(s);
     }
 }

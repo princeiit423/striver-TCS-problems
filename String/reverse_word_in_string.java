ffb95e9eb3f9d0aadc19caf96446a054cb132973
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-09 16:31:39 +0530

    day-12

---
+++ String/reverse_word_in_string.java
@@ -0,0 +1,18 @@
+package String;
+
+public class reverse_word_in_string {
+    public static void reverseWord(String s) {
+        StringBuffer sb = new StringBuffer();
+        String[] arr = s.trim().split("\\s+");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append(arr[i]);
+            sb.append(' ');
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String args[]) {
+        String s = "this is an amazing program";
+        reverseWord(s);
+    }
+}

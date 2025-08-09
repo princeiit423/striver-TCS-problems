503f50f54915474b4f805bb89da04577f4115508
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-09 14:53:01 +0530

    day-12

---
+++ String/count_word_in_String.java
@@ -0,0 +1,14 @@
+package String;
+
+public class count_word_in_String {
+    public static void countWord(String s) {
+        String[] arr = s.trim().split(" ");
+
+        System.out.println(arr.length);
+    }
+
+    public static void main(String[] args) {
+        String s = "hi there";
+        countWord(s);
+    }
+}

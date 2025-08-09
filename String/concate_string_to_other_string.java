b9c1834413d77a9177ddfb5d132b4676a2ae56e4
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-09 15:59:04 +0530

    day-12

---
+++ String/concate_string_to_other_string.java
@@ -0,0 +1,9 @@
+package String;
+
+public class concate_string_to_other_string {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String str2 = "World";
+        System.out.println(str + str2);
+    }
+}

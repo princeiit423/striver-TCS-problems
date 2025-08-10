aee22b5e049d34f7785149c831c9356cc5703053
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-10 10:15:38 +0530

    day-13

---
+++ Sorting/bubble_sort.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+import java.util.*;
+
+public class bubble_sort {
+    public static void bubbleSort(int arr[]) {
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array length: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter " + i + " data: ");
+            arr[i] = sc.nextInt();
+        }
+
+        bubbleSort(arr);
+    }
+}

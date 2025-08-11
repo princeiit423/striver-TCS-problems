bc3171d2a6c561b1d5f6605e41bd6d4d7fb6017d
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-11 10:05:48 +0530

    day-14

---
+++ Sorting/insertion_sort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+public class insertion_sort {
+    public static void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 1, 3, 5, 2 };
+        insertionSort(arr);
+    }
+}

111c7ea4501bba75acb724976b3506b8f4486f41
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-17 18:49:16 +0530

    day-20

---
+++ Numbers/replace_all_zero_with_1_in_integer.java
@@ -0,0 +1,26 @@
+package Numbers;
+
+public class replace_all_zero_with_1_in_integer {
+    public static void replaceZero(int n) {
+        int rev = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit == 0) {
+                digit = 1;
+            }
+            rev = (rev * 10) + digit;
+            n = n / 10;
+        }
+
+        while (rev > 0) {
+            int dig = rev % 10;
+            System.out.print(dig + " ");
+            rev = rev / 10;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 12300;
+        replaceZero(n);
+    }
+}

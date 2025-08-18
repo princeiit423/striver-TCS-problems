f2bbd846d87b2b8116e1e743554c33af871428a9
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-18 09:51:11 +0530

    day-21

---
+++ Recurssion/factorial.java
@@ -0,0 +1,14 @@
+public class factorial {
+    public static int findfact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int fnm1 = findfact(n - 1);
+        return n * fnm1;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(findfact(n));
+    }
+}

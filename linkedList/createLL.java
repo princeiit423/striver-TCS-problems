d44457e3be37526bd68c38aaf1db1d9120f520f6
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-21 19:17:19 +0530

    LL

--- linkedList/createLL.java
+++ linkedList/createLL.java
@@ -1,5 +1,52 @@
 package linkedList;
 
 public class createLL {
+    class Node {
+        String data;
+        Node next;
 
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    public void add(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("No data found");
+            return;
+        }
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + "->");
+            curr = curr.next;
+        }
+        System.out.println("Null");
+    }
+
+    public static void main(String[] args) {
+        createLL list = new createLL();
+        list.add("My");
+        list.add("Name");
+        list.add("is");
+        list.add("Faiz");
+
+        list.print();
+
+    }
 }

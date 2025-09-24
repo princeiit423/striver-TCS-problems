6f2a794b293d7967ba5ad05c82d71604a3c02d61
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-09-24 20:14:44 +0530

    double LL

---
+++ linkedList/doubleLL.java
@@ -0,0 +1,50 @@
+public class doubleLL {
+    class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    Node head;
+    Node tail;
+    int size;
+
+    // add
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+
+        size++;
+    }
+
+    // print
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        doubleLL dl = new doubleLL();
+        dl.addFirst(1);
+        dl.addFirst(2);
+        dl.addFirst(3);
+        dl.addFirst(4);
+        dl.print();
+
+    }
+}
